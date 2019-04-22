e5b792f573a2f7f36421adbe0dbbe1a97214f549
liam5liam <liam.brydon@live.com.au>
2019-04-22 18:54:12 +1200

    added 2d array to model

--- EyeBallMaze/src/Model.java
+++ EyeBallMaze/src/Model.java
@@ -2,8 +2,12 @@ import java.util.ArrayList;
 
 public class Model {
 	
-	ArrayList<ArrayList<String>> array = new ArrayList<ArrayList<String>>();
+	ArrayList<ArrayList<String>> gameMap = new ArrayList<ArrayList<String>>();
 	
+	gameMap[0][0] = new ArrayList<String>(); // needs a new one each time, doesn't need a name
+	gameMap[0][0].add( "bla bla bla" );
+	
+	system.out.println(gameMap);
 	public Int getWidth(){
 		
 	}
