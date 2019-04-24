9185bd998e9bf307cb36ea571870328eaee8f243
liam5liam <liam.brydon@live.com.au>
2019-04-24 17:34:08 +1200

    Movement now works, only need to work goal

--- EyeBallMaze/src/nz/ac/ara/lab0442/EyeBallMaze/Model.java
+++ EyeBallMaze/src/nz/ac/ara/lab0442/EyeBallMaze/Model.java
@@ -1,5 +1,7 @@
 package nz.ac.ara.lab0442.EyeBallMaze;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Scanner;
 
@@ -41,7 +43,7 @@ public class Model implements IGame {
 				String[] item = GameMap[y][x].split("");
 				
 				StringBuilder sb = new StringBuilder(GameMap[y][x]);
-				if (sb.charAt(2) == 'U'){
+				if (sb.charAt(2) != ' '){
 					player.x = x;
 					player.y = y;
 					player.looking = PlayerDirection.get(item[2]);
@@ -71,47 +73,103 @@ public class Model implements IGame {
     }
     
     public Object[] whatsAt(int x, int y){
-    	String[] item = GameMap[y][x].split("");
-    	Shapes shape = Shapes.get(item[0]);
-    	Colours colour = Colours.get(item[1]);
-    	Boolean isGoal = false;
-    	if (item[3] == "G"){
-    		isGoal = true;
+    	Object[] out = new Object[]{};
+    	ArrayList<Object> temp = new ArrayList<Object>(Arrays.asList(out));
+    	if(y > GameMap.length || x > GameMap[0].length || y < 0 || x < 0){
+    		temp.add(false);
+    		System.out.println("Invalid Mo");
+    	} else {
+	    	String[] item = GameMap[y][x].split("");
+	    	Shapes shape = Shapes.get(item[0]);
+	    	Colours colour = Colours.get(item[1]);
+	    	Boolean isGoal = false;
+	    	if (item[3] == "G"){
+	    		isGoal = true;
+	    	}
+	    	temp.add(shape);
+	    	temp.add(colour);
+	    	temp.add(isGoal);
+	    	
     	}
-    	return new Object[]{shape, colour, isGoal};
+		return temp.toArray();
     }
     
     public void checkMove(String move){
-    	String[] arrOfInput = move.split("");
-    	Direction direction = Direction.get(arrOfInput[0]);
+    	String[] theirInput = move.split("");
+    	Direction direction = Direction.get(theirInput[0]);
     	
-    	int spaces = Integer.parseInt(arrOfInput[1]);
+    	int spaces = Integer.parseInt(theirInput[1]);
     	
     	moveCounter++;
     	if (direction == Direction.Down) { this.moveVertical(spaces, PlayerDirection.Down);}
     	if (direction == Direction.Up) { this.moveVertical(-spaces, PlayerDirection.Up);}
+    	
+    	if (direction == Direction.Left) { this.moveHorizontal(-spaces, PlayerDirection.Left);}
+    	if (direction == Direction.Right) { this.moveHorizontal(spaces, PlayerDirection.Right);}
+    }
+    
+    public void moveHorizontal(int spaces, PlayerDirection playerDirection){
+    	int movingTo = player.x + spaces;
+    	
+    	Object[] newLocation = whatsAt(movingTo, player.y);
+    	
+    	if (newLocation.length < 2){
+    		System.out.println("Invalid Move");
+    	}
+    	else {
+	    	Shapes newShape = (Shapes) newLocation[0];
+	    	Colours newColour = (Colours) newLocation[1];
+	    	Boolean isGoal = (Boolean) newLocation[2];
+	    	
+	    	if (this.currentShape == newShape || this.currentColour == newColour){ 
+	    		player.looking = playerDirection;
+	    		
+	    		StringBuilder sb = new StringBuilder(GameMap[player.y][player.x]);
+	    		sb.deleteCharAt(2);
+	    		sb.insert(2, " ");
+	    		GameMap[player.y][player.x] = sb.toString();
+	    		
+	    		System.out.println(sb.toString());
+	    		
+	    		StringBuilder sb2 = new StringBuilder(GameMap[player.y][movingTo]);
+	    		System.out.println(sb2.toString());
+	    		sb2.deleteCharAt(2);
+	    		sb2.insert(2, player.looking.getAbbreviation());
+	    		GameMap[player.y][movingTo] = sb2.toString();
+	    		
+	    		System.out.println(sb2.toString());
+	    		
+	    	} else { System.out.println("Invalid MoveX");}
+    	}
     }
     
     public void moveVertical(int spaces, PlayerDirection playerDirection){
     	int movingTo = player.y + spaces;
+    	
     	Object[] newLocation = whatsAt(player.x, movingTo);
     	
-    	Shapes newShape = (Shapes) newLocation[0];
-    	Colours newColour = (Colours) newLocation[1];
-    	Boolean isGoal = (Boolean) newLocation[2];
-    	
-    	if (this.currentShape == newShape || this.currentColour == newColour){
-    		player.looking = playerDirection;
-    		
-    		StringBuilder sb = new StringBuilder(GameMap[player.y][player.x]);
-    		sb.deleteCharAt(2);
-    		sb.insert(2, " ");
-    		GameMap[player.y][player.x] = sb.toString();
-    		
-    		StringBuilder sb2 = new StringBuilder(GameMap[movingTo][player.x]);
-    		sb2.deleteCharAt(2);
-    		sb2.insert(2, player.looking);
-    		GameMap[movingTo][player.x] = sb2.toString();
+    	if (newLocation.length < 2){
+    		System.out.println("Invalid Move");
+    	}
+    	else {
+	    	Shapes newShape = (Shapes) newLocation[0];
+	    	Colours newColour = (Colours) newLocation[1];
+	    	Boolean isGoal = (Boolean) newLocation[2];
+	    	
+	    	if (this.currentShape == newShape || this.currentColour == newColour){ 
+	    		player.looking = playerDirection;
+	    		
+	    		StringBuilder sb = new StringBuilder(GameMap[player.y][player.x]);
+	    		sb.deleteCharAt(2);
+	    		sb.insert(2, " ");
+	    		GameMap[player.y][player.x] = sb.toString();
+	    		
+	    		StringBuilder sb2 = new StringBuilder(GameMap[movingTo][player.x]);
+	    		sb2.deleteCharAt(2);
+	    		sb2.insert(2, player.looking.getAbbreviation());
+	    		GameMap[movingTo][player.x] = sb2.toString();
+	    		
+	    	} else { System.out.println("Invalid Move");}
     	}
     }
     
